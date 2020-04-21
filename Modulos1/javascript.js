27c87b8a7049aae7698fa24daf7b16783d387c05
Sandra <sandrakawakame@gmail.com>
2020-04-21 15:50:40 -0300

    Modulos com
    Rápida introdução de como usar módulos na parte do backend utilizando NodeJS
    
    Passos:
    Acessar o terminal de comando CMD e acessar a pasta do projeto
    
    instalar o express que é um mini framework de javascript com o comando
    npm install --save express
    
    comando para rodar o arquivo
    node javascript.js
    
    Material de Referencia:
    https://webdesignemfoco.com/cursos/javascript/vanilla-javascript-36-modulos
    
    No node chama-se os módulos através do require que ficam na pasta node_modules

--- Modulos1/javascript.js
+++
@@ -1,5 +0,0 @@
-import {sum, mult, percent} from "./modules/calc";
-
-console.log(sum(10,9));
-console.log(mult(10,9));
-console.log(percent(10,100));
--- ProgressBarJavascript/controller.php
+++
@@ -1,3 +0,0 @@
-<?php
-$files=$_FILES['files'];
-move_uploaded_file($files['tmp_name'],"C:/wamp/www/ProgressBarJavascript/$files[name]");
